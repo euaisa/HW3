791e6e20d6259d01c53f01f674c16f8f0062db98
Isa <110069448+euaisa@users.noreply.github.com>
2023-01-22 14:48:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+let origArray = [2,3,4];
+
+document.addEventListener("DOMContentLoaded", function(){
+    for(let i = 0; i < origArray.length; i++){
+        document.getElementById("textbox" + (i+1)).value = origArray[i];
+    }
+
+    document.getElementById("button1").onclick = function(){
+        let newArray = origArray.map(function(val){
+            return val*val;
+        });
+        for(let i = 0; i < newArray.length; i++){
+            document.getElementById("textbox" + (i+1)).value = newArray[i];
+        }
+    }
+
+    document.getElementById("button2").addEventListener("click", function(){
+        let newArray = origArray.map(function(val){
+            return val*val*val;
+        });
+        for(let i = 0; i < newArray.length; i++){
+            document.getElementById("textbox" + (i+1)).value = newArray[i];
+        }
+    });
+
+    document.getElementById("button3").addEventListener("click", fourthPower);
+});
+
+function fourthPower(){
+    let newArray = origArray.map(function(val){
+        return val*val*val*val;
+    });
+    for(let i = 0; i < newArray.length; i++){
+        document.getElementById("textbox" + (i+1)).value = newArray[i];
+    }
+}
